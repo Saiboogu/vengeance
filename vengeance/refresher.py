a05642d24e765f59717935761f502aafeed42fad
Sean Wallitsch <shidarin@alphamatte.com>
2014-05-30 22:20:26 -0700

    Bug fixes and refactoring

--- vengeance/refresher.py
+++ vengeance/refresher.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python
+from datetime import datetime
 from time import sleep
 from bs4 import BeautifulSoup
-import re
 import requests
-import json
 
-posters = []
-baseurl = "http://www.mondotees.com/"
-edition_regex = re.compile(r'Edition of (\d+)')
 
-while True:
-  request = requests.get(baseurl)
-  main_soup = BeautifulSoup(request.text)
-  available =\
-    [baseurl+a.get("href") for a in main_soup.find("ul").findAll("a")]
+def refresh_page(page, rate=5):
+    """Refreshes a page and checks for change in available links"""
+    previous = []
+    first_run = True
 
-  if posters != []:
-    oldposters = posters
+    while True:
+        start_time = datetime.utcnow()
+        request = requests.get(page)
+        main_soup = BeautifulSoup(request.text)
+        links = main_soup.find_all("li")
+        available = [link.find("a").get("href") for link in links]
+        available.sort()
 
-  posters = []
+        if not first_run and available != previous:
+            # If this isn't our first run and the new result doesn't match
+            # previous, we've got new drops!
+            if available:
+                return available
+            else:
+                # If available is now empty, we need to empty out previous
+                # So we can tell when a drop is back on.
+                previous = available
+        elif first_run:
+            # This was our first check, so we'll set our compare result
+            previous = available
+            first_run = False
 
-  for url in available:
-    new_soup = BeautifulSoup(requests.get(url).text)
-    poster = {
-      "available": new_soup.find(id="availability").text.find("Sold Out") < 0,
-      "url": url,
-      "price": new_soup.find(class_="price").text,
-      "title": new_soup.find(class_="page_headers").text,
-    }
+        # If our available list still matches our previous list, we just wait
+        # a little bit then loop again.
 
-    try:
-      poster["editionof"] =\
-        int(edition_regex.findall(new_soup.find(class_="item").text)[0])
-    except:
-      pass
+        # We should make sure we're refreshing the page at most every 5 seconds
+        # but don't wait if we've taken longer than 5 seconds.
+        duration = datetime.utcnow() - start_time
+        if duration.seconds < rate:
+            sleep(rate - duration.seconds)
 
-    posters.append(poster)
-  
-  print json.dumps(posters, indent=2)
-  sleep(30)
+
+print refresh_page('http://www.mondotees.com/')
